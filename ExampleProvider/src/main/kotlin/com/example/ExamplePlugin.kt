1dcbfe35b7d364e50d7935d62f69151d7e50cd7e
DistantCoder <82241815+WellCodeIsDelicious@users.noreply.github.com>
2024-01-04 15:55:33 +0000

    Update ExamplePlugin.kt

--- ExampleProvider/src/main/kotlin/com/example/ExamplePlugin.kt
+++ ExampleProvider/src/main/kotlin/com/example/ExamplePlugin.kt
@@ -1,24 +1,13 @@
-package com.example
+package com.lagradost
 
 import com.lagradost.cloudstream3.plugins.CloudstreamPlugin
 import com.lagradost.cloudstream3.plugins.Plugin
-import com.lagradost.cloudstream3.APIHolder
 import android.content.Context
-import android.util.Log
-import androidx.appcompat.app.AppCompatActivity
 
 @CloudstreamPlugin
-class TestPlugin: Plugin() {
-    var activity: AppCompatActivity? = null
-
+class DistantIPTVProviderPlugin: Plugin() {
     override fun load(context: Context) {
-        activity = context as AppCompatActivity
-        // All providers should be added in this manner
-        registerMainAPI(ExampleProvider(this))
-
-        openSettings = { ctx ->
-            val frag = BlankFragment(this)
-            frag.show(activity!!.supportFragmentManager, "sexFrag")
-        }
+        // All providers should be added in this manner. Please don't edit the providers list directly.
+        registerMainAPI(DistantIPTVProvider())
     }
 }
